044313eb59136529ce9599a06b7984973f6fc840
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-01-22 22:47:30 +0100

    feat: create LikeList and LikeDetail generic views

--- likes/serializers.py
+++ likes/serializers.py
@@ -4,15 +4,46 @@ from .models import Like
 
 class LikeSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    post_info = serializers.SerializerMethodField()
+
+    def get_post_info(self, obj):
+        return {
+            'username': obj.post.owner.username,
+            'title': obj.post.title
+        }
 
     class Meta:
         model = Like
-        fields = ['id', 'created_at', 'owner', 'post' ]
+        fields = ['id', 'created_at', 'owner', 'post', 'post_info' ]
 
+    # The create function in your LikeSerializer class is an overridden method
+    # from Django's ModelSerializer class, which is used to handle the creation
+    # of new instances of the Like model based on the validated data received
+    # from an API request.
     def create(self, validated_data):
         try:
+            # super().create(validated_data) calls the create method of the
+            # superclass (ModelSerializer in this case). This method takes the
+            # validated_data - which is a dictionary containing deserialized
+            # input data - and creates a new instance of the Like model. The
+            # validated_data would typically include the fields necessary to
+            # create a Like instance, such as the user (owner) and the post
+            # being liked (post).
             return super().create(validated_data)
+        # Handling IntegrityError: The try...except block is used to catch any
+        # IntegrityError that might be raised during the creation of a new Like
+        # instance. An IntegrityError would occur if the creation violates any
+        # database constraints. In your case, the most likely cause of an
+        # IntegrityError is the violation of the unique_together constraint in
+        # the Like model, which ensures that a user cannot like the same post
+        # more than once.
         except IntegrityError:
+            # If an IntegrityError is caught, the function raises a
+            # serializers.ValidationError. This is a custom error handling to
+            # provide a more specific and user-friendly error message. In your
+            # code, it specifies the error with the message
+            # {'detail': 'possible duplicate'}, indicating that the like action
+            # might be a duplicate (i.e., the user has already liked the post).
             raise serializers.ValidationError({
                 'detail': 'possible duplicate',
             })
--- likes/views.py
+++ likes/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import permissions, generics
+from drf_api.permissions import IsOwnerOrReadOnly
+from .serializers import LikeSerializer
+from .models import Like
 
-# Create your views here.
+class LikeList(generics.ListCreateAPIView):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    # The primary purpose of overriding perform_create is to add custom behavior
+    # during the save operation of a model instance. In your case, when a new Like
+    # instance is created, you need to assign the owner of the Like to the current
+    # authenticated user making the request. This cannot be done automatically by
+    # the serializer since the owner field is a ReadOnlyField in your LikeSerializer.
+
+    # perform_create is a powerful way to include additional logic when creating new
+    # instances of a model via a DRF view. In your case, it's essential for correctly
+    # associating each new Like with the authenticated user who created it
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+    permission_classes = [IsOwnerOrReadOnly]
